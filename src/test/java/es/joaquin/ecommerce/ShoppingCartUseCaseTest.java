7360180197ffc13b2e59e2d246bdfdacfc3cc6c5
joaquin <jdevicente@genasys.com>
2021-02-24 02:19:36 +0100

    Added first test

---
+++ src/test/java/es/joaquin/ecommerce/ShoppingCartUseCaseTest.java
@@ -0,0 +1,19 @@
+package es.joaquin.ecommerce;
+
+public class ShoppingCartUseCaseTest {
+
+	/*@Mock
+	private ShoppingCartRepository shoppingCartRepository;
+
+	@Mock
+	private ProductRepository productRepository;
+
+	@InjectMocks
+	CartUseCaseImpl cartUseCase;
+
+	@BeforeEach
+	public void before() {
+		initMocks(this);
+	}*/
+
+}
